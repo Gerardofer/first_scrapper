acd9f13d51f52a53055d7d17f7264ab25bed835d
Gerardo Fernandez <gerardojose13@gmail.com>
2018-03-12 23:17:03 -0700

    adds scrap request and storage to the database

---
+++ models/Headline.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+var HeadlineSchema = new mongoose.Schema({
+    link: {
+        type: String,
+        required: true
+    },
+    body: {
+        type: String,
+        required: true
+    },
+    note: {
+        type: Schema.Types.ObjectId,
+        ref: "Note"
+    }
+});
+
+var Headline = mongoose.model("Headline", HeadlineSchema);
+
+module.exports = Headline;
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,9 +1,34 @@
+const request = require("request");
+const cheerio = require("cheerio");
+
+var db = require("../models");
+
+
 module.exports = (app) => {
     app.get("/", (req, res) => {
         res.redirect("/articles");
     });
 
     app.get("/articles", (req, res) => {
-        res.render("index")
-    });
+        request("https://www.nytimes.com", function(err, response, html) {
+
+            var $ = cheerio.load(html);
+
+            var result = {};
+
+            $("article h2").each(function(i, element) {
+
+                result.link = $(element).children("a").attr("href");
+                result.body = $(element).children("a").text();
+
+                db.Headline.create(result)
+                    .then(function(dbResult) {
+                        console.log(dbResult);
+                        res.render("index", { results: result });
+                    });
+                // console.log(result);
+            })
+        });
+
+    })
 }
