265dccb5c7078259daf566561af2f2d582ffdb0e
Gerardo Fernandez <gerardojose13@gmail.com>
2018-03-12 23:50:14 -0700

    adds POST route

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 const app = express();
 
 const PORT = process.env.PORT || 8080;
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines"
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI, {
+    useMongoClient: true
+})
 
 require("./routes/apiRoutes.js")(app);
 
