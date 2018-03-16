34a0d584d54ab288b8197c036ae4087ad0180444
Gerardo Fernandez <gerardojose13@gmail.com>
2018-03-16 14:00:09 -0700

    Update server.js
    Adds the GET request to the database to find the saved articles

--- server.js
+++ server.js
@@ -71,29 +71,30 @@ app.get("/scrapped", (req, res) => {
     res.redirect("/articles")
 });
 
+//  GET route to update the "saved" property to "true".
+app.get("/saved/:id", (req, res) => {
+    let id = req.params.id
+    Headline.findByIdAndUpdate(id, {$set : {saved : true}}, (err, savedArticle) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.redirect("/articles");
+        }
+    })
+})
+
 //  -----------------  GET route to get all "Saved" articles ----------------------//
+
 app.get("/articles/saved", (req, res) => {
-    Headline.find().where({saved: true}, (err, savedArticle) => {
+    Headline.find({saved: true}, (err, savedArticle) => {
         if(err) {
             res.redirect("/articles");
         } else {
-            res.render("show", {savedArtice: savedArticle});
+            res.render("show", {savedArticle: savedArticle});
         }
     });
 });
 
-//  -------------------  PUT route to update article to "save" specific item  -----------------  //
-//Check put statement
-app.put("/articles/save/:id", (req, res) => {
-    let id = req.params.id;
-    Headline.findByIdAndUpdate(id, {$set: {saved: true}}, (err, articleUpdate) => {
-        if (err) {
-            res.redirect("/articles");
-        } else {
-            res.redirect("/articles/saved");
-        }
-    });
-});
 
 app.listen(PORT, () => {
     console.log("server listening on port", PORT);
